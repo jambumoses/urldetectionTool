ee2a329a309e0120229a17ca3f9f4e12c066219d
jambumoses <jambcareer@gmail.com>
2024-09-15 16:02:35 +0300

    doc template

--- base/urls.py
+++ base/urls.py
@@ -9,27 +9,6 @@ urlpatterns = [
     path('docs', views.docs, name="docs"),
     path('search/', views.search, name="search"),
     path('result/<int:id>', views.singleResult, name="result"),
-    #category
-    #path('tag/<str:category>', views.categories, name="categories"),
-    #details / news
-    #path('articles/', views.topArticles, name="topArticles"),
-    #path('article/<str:headline>', views.articles, name="articles"), #(P<article_id>\w{0,50})/$
-    #path('article/<str:headline>/video', views.videoArticles, name="videoArticles"),
-    # all about a specific sport
-    #path('sports/', views.sportsPage, name="sportspage"),
-    #path('sports/<str:sport>', views.sports, name="sports"),
-    #about the author and related publishes
-    #path('author/<str:username>', views.author, name="author"),
-    
-    #contact
-    #path('contact', views.contact, name="contact"),
-
-    #news page
-    #path('news/', views.news, name="news"),
-
-    #path('entertainment/', views.entertainment, name="entertainment"),
-    #path('entertainment/<str:tag>', views.entertainmentSpecific, name="entertainmentSpecific"),
-
 ]
 
 #TODO
